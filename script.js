298ee25ddae5385492bd075da96d5d78fc26cd2d
Kanwar Haseeb Ahmad <themhcode@gmail.com>
2022-04-23 22:37:10 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Foot
 function fToCentiMeters(valNum) {
     document.getElementById("outputCentiMeters").innerHTML = valNum * 30.48;
 }
@@ -16,3 +17,41 @@ function fToKiloMeters(valNum) {
 function fToMiles(valNum) {
     document.getElementById("outputMiles").innerHTML = valNum * 0.00018939;
 } 
+// CM
+function cmToFoot(valNum) {
+    document.getElementById("outputCMToFoot").innerHTML = valNum * 30.48;
+}
+function cmToMeters(valNum) {
+    document.getElementById("outputCMToMeters").innerHTML = valNum / 100;
+}
+function cmToInches(valNum) {
+    document.getElementById("outputCMToInches").innerHTML = valNum / 2.54;
+}
+function cmToYards(valNum) {
+    document.getElementById("outputCMToYards").innerHTML = valNum / 91.44;
+}
+function cmToKiloMeters(valNum) {
+    document.getElementById("outputCMToKiloMeters").innerHTML = valNum / 100000;
+}
+function cmToMiles(valNum) {
+    document.getElementById("outputCMToMiles").innerHTML = valNum / 160934;
+}
+// Meter
+function mToCentiMeters(valNum) {
+    document.getElementById("outputMToCentiMeters").innerHTML = valNum * 100;
+}
+function mToFoot(valNum) {
+    document.getElementById("outputMToFoot").innerHTML = valNum * 3.281;
+}
+function mToInches(valNum) {
+    document.getElementById("outputMToInches").innerHTML = valNum * 39.37;
+}
+function mToYards(valNum) {
+    document.getElementById("outputMToYards").innerHTML = valNum * 1.094;
+}
+function mToKiloMeters(valNum) {
+    document.getElementById("outputMToKiloMeters").innerHTML = valNum / 1000;
+}
+function mToMiles(valNum) {
+    document.getElementById("outputMToMiles").innerHTML = valNum / 1609;
+}
